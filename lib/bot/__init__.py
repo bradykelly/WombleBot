bdd9374ad5ed4c9f2124b08ae0732b245044890e
Brady Kelly <brady@bradykelly.net>
2021-04-26 19:26:57 +0200

    cooldown

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,20 +1,59 @@
+from asyncio.tasks import sleep
+from glob import glob
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord.errors import Forbidden
 from discord.ext.commands import Bot as BotBase
+from discord import Intents
+from discord.ext.commands.context import Context
+from discord.ext.commands.errors import BadArgument, CommandNotFound, CommandOnCooldown, MissingRequiredArgument
+#from lib.db.database import Database
 
 DEFAULT_PREFIX = "`"
-OWNDER_IDS = ["695627499891065033"]
+OWNDER_IDS = [695627499891065033]
+STD_OUT = 835859792924114954
+COGS = [path.split("\\")[-1][:-3] for path in glob("./lib/cogs/*.py")]
+IGNORE_EXCEPTIONS = (CommandNotFound,)
+
+class Ready(object):
+    def __init__(self):
+        for cog in COGS:
+            setattr(self, cog, False)
+
+    def ready_up(self, cog):
+        setattr(self, cog, True)
+        print(f"  {cog} cog ready")
+
+    def all_ready(self):
+        return all([getattr(self, cog) for cog in COGS])
 
 class Bot(BotBase):
     def __init__(self):
-        self.PREFIX = DEFAULT_PREFIX
         self.ready = False
+        self.cogs_ready = Ready()
         self.guild = None
         self.scheduler = AsyncIOScheduler
 
-        super().__init__(command_prefix=DEFAULT_PREFIX, ownder_ids=OWNDER_IDS)
+        # with open("./lib/bot/dsn", "r", encoding="utf-8") as df:
+        #     dsn = df.read()
+        #     self.db = Database(self, dsn)
+
+        super().__init__(
+            command_prefix=DEFAULT_PREFIX, 
+            ownder_ids=OWNDER_IDS,
+            intents=Intents.all())
+
+    def setup(self):
+        for cog in COGS:
+            self.load_extension(f"lib.cogs.{cog}")
+            print(f"  {cog} loaded")
+
+        print("Setup complete")
         
     def run(self, version):
         self.version = version
+
+        print("Running setup")
+        self.setup()
 
         with open("./lib/bot/token", "r", encoding="utf-8") as tf:
             self.token = tf.read()
@@ -22,22 +61,70 @@ class Bot(BotBase):
         print("Running bot...")
         super().run(self.token, reconnect=True)
 
+    async def process_commands(self, message):
+        ctx = await self.get_context(message, cls=Context)
+
+        if ctx.command is not None and ctx.guild is not None:
+            if self.ready:            
+                await self.invoke(ctx)
+
+            else:
+                await ctx.send("I'm not ready to take commands. Please wait a few seconds.")
+
     async def on_connect(self):
         print("Bot connected")    
 
     async def on_disconnect(self):
         print("Bot disconnected")
 
+    async def on_error(self, err, *args, **kwargs):
+        if err == "on_command_error":
+            await args[0].send("Something went wrong.")
+
+        await self.stdout.send("An error occurred: " + err)        
+        raise
+
+    async def on_command_error(self, ctx, exc):
+        if any([isinstance(exc, error) for error in IGNORE_EXCEPTIONS]):
+            pass
+
+        if isinstance(exc, MissingRequiredArgument):
+            await ctx.send("One or more required arguments are missing.")
+
+        elif isinstance(exc, CommandOnCooldown):
+            await ctx.send(f"That command is on {str(exc.cooldown.type).split('.')[-1]} cooldown. Try again in {exc.retry_after:,.2f} seconds.")
+
+        elif hasattr(exc, "original"):
+            if isinstance(exc.original, Forbidden):
+                await ctx.send("I do not have permission to do that")
+                
+            else:
+                raise exc.original
+
+        else:
+            raise exc
+
     async def on_ready(self):
         if not self.ready:
+            self.stdout = self.get_channel(STD_OUT)
+
+            while not self.cogs_ready.all_ready():
+                await sleep(0.5)
+
+            guild_list = "WombleBot is online in guilds: \n"
+            for guild in self.guilds:
+                guild_list += "\t  " + guild.name + "\n"
+            await self.stdout.send(guild_list)
+
             self.ready = True
-            print("Bot ready")
+            print("Bot ready")            
 
         else:
             print("Bot reconnected")
 
     async def on_message(self, msg):
-        pass
+        if not msg.author.bot:
+            await self.process_commands(msg)
 
 
 bot = Bot()
---
+++ lib/cogs/fun.py
@@ -0,0 +1,34 @@
+from aiohttp import request
+from discord.ext.commands import command, cooldown
+from discord.ext.commands import Cog, BucketType
+
+
+class Fun(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command(name="fact")
+    @cooldown(1, 60, BucketType.guild)
+    async def fact_command(self, ctx, animal: str):
+        if animal.lower() in ("dog", "cat", "panda", "fox", "bird", "koala"):
+            url = f"https://some-random-api.ml/facts/{animal.lower()}"
+
+            async with request("GET", url, headers={}) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    await ctx.send(data["fact"])
+
+                else:
+                    await ctx.send(f"The Facts API returned a {response.status} status code.")
+
+        else:
+            await ctx.send("No facts available for that animal.")
+
+    @Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up("fun")
+
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
