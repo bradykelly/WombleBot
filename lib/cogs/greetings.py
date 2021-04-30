15825ae4b0bb22036ccccd941eee3fb452216b25
Brady Kelly <brady@bradykelly.net>
2021-04-30 15:17:49 +0200

    on_member_remove

--- lib/cogs/greetings.py
+++ lib/cogs/greetings.py
@@ -3,7 +3,7 @@ from discord.ext.commands import Cog
 from discord.ext.commands.core import guild_only
 
 
-DEFAULT_WELCOME = 836528471328161812
+DEFAULT_GREETING = 836528471328161812
 DEFAULT_GENERAL = 836987836498575370
 class Greetings(Cog):
     def __init__(self,bot):
@@ -19,10 +19,10 @@ class Greetings(Cog):
         await self.bot.db.execute("INSERT INTO experience (guild_id, user_id) VALUES ($1, $2) " +
             "ON CONFLICT(guild_id, user_id) DO NOTHING", member.guild.id, member.id)
 
-        record = await self.bot.db.record("SELECT welcome_chan_id, general_chan_id FROM config WHERE guild_id = $1", member.guild.id)
-        welcome = record["welcome_chan_id"] or DEFAULT_WELCOME
+        record = await self.bot.db.record("SELECT greetings_chan_id, general_chan_id FROM config WHERE guild_id = $1", member.guild.id)
+        greeting = record["greetings_chan_id"] or DEFAULT_GREETING
         general = record["general_chan_id"] or DEFAULT_GENERAL
-        await self.bot.get_channel(welcome).send(f"Welcome to **{member.guild.name}** {member.mention}! "
+        await self.bot.get_channel(greeting).send(f"Welcome to **{member.guild.name}** {member.mention}! "
             + f"Head over to <#{general}> to say hello.")
         await self.bot.get_channel(general).send(f"{member.mention} has arrived!")
 
@@ -40,15 +40,17 @@ class Greetings(Cog):
 
             except ValueError:
                 pass     
-                   
+
             else:
                 to_add = [member.guild.get_role(int(role)) for role in roles_list]
                 await member.add_roles(*to_add)
                 
     @Cog.listener()
-    async def on_member_leave(self, member):
-        pass
-
+    async def on_member_remove(self, member):
+        await self.bot.db.execute("DELETE FROM experience WHERE guild_id = $1 AND user_id = $2", member.guild.id, member.id)
+        greeting = await self.bot.db.field("SELECT greetings_chan_id FROM config WHERE guild_id = $1", member.guild.id)
+        greeting = greeting or DEFAULT_GREETING
+        await self.bot.get_channel(greeting).send(f"{member.display_name} has left {member.guild.name}")
 
 def setup(bot):
     bot.add_cog(Greetings(bot))            
